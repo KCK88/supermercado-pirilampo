8c38d23b7f5654dcaacb2204cb84759fa1621006
Claudio Meira <claudiomeirabs@gmail.com>
2023-11-30 13:43:09 -0300

    Requisito 3 feito e testado

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  let outOfStockProducts = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock === 0) {
+      outOfStockProducts.push(stockProducts[index].productName);
+    }
+  }
+  return outOfStockProducts;
 };
-
+console.log(getOutOfStockProducts());
 module.exports = { getOutOfStockProducts };
