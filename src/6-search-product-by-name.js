88aaf46296c2072de9a53d024db3f5939d28d4f5
Claudio Meira <claudiomeirabs@gmail.com>
2023-12-02 21:40:49 -0300

    Requisito 6 feito e testado

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (string) => {
+  let descriptionAndPrice = {};
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].productName === string) {
+      descriptionAndPrice.description = stockProducts[index].description;
+      descriptionAndPrice.formattedPrice = `R$ ${stockProducts[index].price}`;
+      return descriptionAndPrice;
+    }
+  }
+  return null;
 };
-
 module.exports = { searchProductByName };
+
+// let object = {};
+// console.log(object.name = 'air max');
+// console.log(object.condition = 'new');
+// console.log(object);
