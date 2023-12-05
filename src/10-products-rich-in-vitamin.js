dab0444cc6f28dca916d42feecf3b28d18e6c091
Claudio Meira <claudiomeirabs@gmail.com>
2023-12-04 23:10:24 -0300

    Requisito 10 feito e testado

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,22 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  const productsRichInVitamin = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    const descriptionPrice = {};
+    if (stockProducts[index].nutritionalInfo.vitamins) {
+      descriptionPrice.description = stockProducts[index].description;
+      descriptionPrice.formattedPrice = `R$ ${stockProducts[index].price}`;
+      const vitamins = Object.entries(stockProducts[index].nutritionalInfo.vitamins);
+      const vitaminsArray = [];
+      for (let index0 = 0; index0 < vitamins.length; index0 += 1) {
+        vitaminsArray.push(`${vitamins[index0][0]} - ${vitamins[index0][1]}`);
+      }
+      descriptionPrice.vitaminsInformation = vitaminsArray;
+      productsRichInVitamin.push(descriptionPrice);
+    }
+  }
+  return productsRichInVitamin;
 };
-
+console.log(getProductsRichInVitamin());
 module.exports = { getProductsRichInVitamin };
